579f016607e573bee6f54d4a00729ab04d166286
Mentra20 <yann.martindes@orange.fr>
2022-01-28 23:00:48 +0100

    modify model to accept analogical condition

--- ProjectJS/src/model/main.js
+++ ProjectJS/src/model/main.js
@@ -1,8 +1,9 @@
 import Action from "./Action.js";
 import Actuator from "./Actuator.js";
+import AnalogicalCondition from "./AnalogicalCondition.js";
 import App from "./App.js";
+import DigitalCondition from "./DigitalCondition.js";
 import Sensor from "./Sensor.js";
-import SensorCondition from "./SensorCondition.js";
 import SensorTransition from "./SensorTransition.js";
 import SIGNAL from "./SIGNAL.js";
 import State from "./State.js";
@@ -10,14 +11,14 @@ import TemporalTransition from "./TemporalTransition.js";
 
 
 let testSensor = new Sensor("button1",5);
-let testSensor2 = new Sensor("button2",5);
+let testSensor2 = new Sensor("captor1",5);
 
 let testLed = new Actuator("led",1);
 
 let ledAction = new Action(testLed,SIGNAL.LOW);
 
-let sensorConditions = [new SensorCondition(testSensor,SIGNAL.HIGH),new SensorCondition(testSensor2,SIGNAL.LOW)]
-let sensorConditions2 = [new SensorCondition(testSensor,SIGNAL.LOW),new SensorCondition(testSensor2,SIGNAL.HIGH)]
+let sensorConditions = [new DigitalCondition(testSensor,SIGNAL.HIGH),new DigitalCondition(testSensor2,SIGNAL.LOW)]
+let sensorConditions2 = [new DigitalCondition(testSensor,SIGNAL.LOW),new AnalogicalCondition(testSensor2,40,true)]
 
 let testTransition = new SensorTransition(undefined,sensorConditions);
 let testTransition2 = new SensorTransition(undefined,sensorConditions2);
@@ -31,7 +32,7 @@ testTransition.next=testState;
 testTransition2.next=testState;
 
 let testTemporalTransition = new TemporalTransition(testState,800);
-let testState2 = new State("state2",[new SensorTransition(testState,[new SensorCondition(testSensor,SIGNAL.HIGH)]),testTemporalTransition],actions);
+let testState2 = new State("state2",[new SensorTransition(testState,[new DigitalCondition(testSensor,SIGNAL.HIGH)]),testTemporalTransition],actions);
 
 let app = new App("app1",[testSensor,testSensor2,testLed],[testState,testState2],testState);
 
