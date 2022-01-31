0794ee05a365714de11e0176ac1f0589fab22337
Valentin Campello <valentin.campello@etu.univ-cotedazur.fr>
2022-01-31 21:45:52 +0100

    AnalogicalBricks

---
+++ ProjectJS/src/exemple/AnalogicalBricks/AnalogicalBricks.ts
@@ -0,0 +1,25 @@
+import SIGNAL from "../../kernel/model/SIGNAL.enum";
+import AppBuilder from "../../embedded/AppBuilder";
+import GenerateArduinoVisitor from "../../kernel/GenerateArduinoVisitor";
+
+export function analogicalBricks(){
+    let app = new AppBuilder("MAIN_APP").withInitialState("off")
+        .addSensor("CAPTOR").onPin(7)
+        .addActuator("BUZZER").onPin(12)
+        .beginState("off")
+        .set("BUZZER").to(SIGNAL.LOW)
+        .when("CAPTOR").isGreaterThan(330).goToState("on")
+        .endState()
+        .beginState("on")
+        .set("BUZZER").to(SIGNAL.HIGH)
+        .when("CAPTOR").isLowerThan(330).goToState("off")
+        .endState()
+        .createModel()
+
+
+
+
+    const generator = new GenerateArduinoVisitor();
+    const inoString = app.accept(generator)
+    console.log(inoString);
+}
