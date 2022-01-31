8ccdfe672704e6c51331150c564bca8db7da247a
Valentin Campello <valentin.campello@etu.univ-cotedazur.fr>
2022-01-31 21:25:12 +0100

    DualCheckAlarm

---
+++ ProjectJS/src/exemple/DualCheckAlarm/DualCheckAlarm.ts
@@ -0,0 +1,25 @@
+import SIGNAL from "../../kernel/model/SIGNAL.enum";
+import AppBuilder from "../../embedded/AppBuilder";
+import GenerateArduinoVisitor from "../../kernel/GenerateArduinoVisitor";
+
+export function dualCheckAlarm(){
+    const app = new AppBuilder("MAIN_APP").withInitialState("off")
+        .addSensor("BUTTON2").onPin(8)
+        .addSensor("BUTTON").onPin(9)
+        .addActuator("BUZZER").onPin(11)
+        .beginState("off")
+        .set("BUZZER").to(SIGNAL.LOW)
+        .when("BUTTON").is(SIGNAL.HIGH).andWhen("BUTTON2").is(SIGNAL.HIGH).goToState("on")
+        .endState()
+        .beginState("on")
+        .set("BUZZER").to(SIGNAL.HIGH)
+        .when("BUTTON").is(SIGNAL.LOW).goToState("off")
+        .when("BUTTON2").is(SIGNAL.LOW).goToState("off")
+        .endState()
+        .createModel()
+
+
+    const generator = new GenerateArduinoVisitor();
+    const inoString = app.accept(generator)
+    console.log(inoString);
+}
--- ProjectJS/src/exemple/DualCheckAlarm/DualCheckAlarmExternal.ino
+++ ProjectJS/src/exemple/DualCheckAlarm/DualCheckAlarmExternal.ino
@@ -9,33 +9,41 @@ STATE currentState = off;
 
 boolean buttonBounceGuard = false;
 long buttonLastDebounceTime = 0;
+boolean button2BounceGuard = false;
+long button2LastDebounceTime = 0;
 
 void setup(){
-	pinMode(2, OUTPUT);  // led [Actuator]
 	pinMode(3, OUTPUT);  // buzzer [Actuator]
 	pinMode(1, INPUT);  // button [Sensor]
+	pinMode(2, INPUT);  // button2 [Sensor]
 }
 
 void loop(){
 	switch(currentState){
 		case off :
-			digitalWrite(2,LOW);
 			digitalWrite(3,LOW);
 			buttonBounceGuard = millis() - buttonLastDebounceTime > debounce;
+			button2BounceGuard = millis() - button2LastDebounceTime > debounce;
 
-			if(digitalRead(1) == HIGH && buttonBounceGuard){
+			if(digitalRead(1) == HIGH && buttonBounceGuard && digitalRead(2) == HIGH && button2BounceGuard){
 				buttonLastDebounceTime = millis();
+				button2LastDebounceTime = millis();
 				currentState = on;
 			}
 		break;
 
 		case on :
-			digitalWrite(2,HIGH);
 			digitalWrite(3,HIGH);
 			buttonBounceGuard = millis() - buttonLastDebounceTime > debounce;
+			button2BounceGuard = millis() - button2LastDebounceTime > debounce;
 
 			if(digitalRead(1) == LOW && buttonBounceGuard){
 				buttonLastDebounceTime = millis();
+				currentState = off;
+			}
+
+			if(digitalRead(2) == LOW && button2BounceGuard){
+				button2LastDebounceTime = millis();
 				currentState = off;
 			}
 		break;
